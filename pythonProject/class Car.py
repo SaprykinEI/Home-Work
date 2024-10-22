6c3774a98eaf865ff4c02f6e55afbd515dccf594
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-10-22 17:24:10 +0300

    Создали класс Battery который выводит информацию о батареи для эклектроавто

--- pythonProject/class Car.py
+++ pythonProject/class Car.py
@@ -51,3 +51,16 @@ print(my_new_car.get_descriptive_name())
 my_new_car.update_odometer(23)
 my_new_car.update_odometer(24)
 print(my_new_car.read_odometer())
+
+
+
+class Battery():
+    '''Простая модель акумулятора автомобиля'''
+
+    def __init__(self, battery_size=75):
+        '''Инициализирует атрибуты аккумулятора'''
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''Выводит информацию о мощности аккумулятора'''
+        print(f"This car has a {self.battery_size}-kwh battery")
