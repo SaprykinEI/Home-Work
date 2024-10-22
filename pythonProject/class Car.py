9155a452f4e5ef559ae3e863c978bd7a2e8c18b1
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-10-22 17:18:30 +0300

    Добавили метод одометр, который выводит пробег

--- pythonProject/class Car.py
+++ pythonProject/class Car.py
@@ -7,10 +7,15 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
-
+        self.odometer_reading = 0
 
 
     def get_descriptive_name(self):
         '''Возвращает аккуратно отформатированное описание'''
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+
+
+    def read_odometer(self):
+        '''Вывод пробег машины в милях'''
+        print(f"This car has {self.odometer_reading} miles on it")
