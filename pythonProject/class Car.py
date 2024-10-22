9ee5892d71780a112d38476707f5c73f99c2b97b
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-10-22 17:20:43 +0300

    Создали несколько экземпляров класса Car

--- pythonProject/class Car.py
+++ pythonProject/class Car.py
@@ -32,3 +32,22 @@ class Car():
     def increment_odometer(self, miles):
         '''Увеличивает показания одометра с заданным приращением'''
         self.odometer_reading += miles
+
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+
+
+
+my_new_car = Car('audi', 'a4', '2019')
+print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23)
+my_new_car.update_odometer(24)
+print(my_new_car.read_odometer())
